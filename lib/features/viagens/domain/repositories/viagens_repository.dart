a32715b6b74b4496c90d85396393eee056255a7b
Filipi Inácio Santos <filipiinacio25251120@gmail.com>
2026-07-02 19:44:59 -0300

    fix: removing wrong files and folders

--- lib/features/viagens/domain/repositories/viagens_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../entities/corrida.dart';
-
-abstract class ViagensRepository {
-  Future<List<Corrida>> buscarViagensPorSemana({
-    required DateTime inicioSemana,
-    required DateTime fimSemana,
-  });
-}
