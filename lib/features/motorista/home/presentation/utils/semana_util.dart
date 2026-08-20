db5a12032a5f8f6db7f9027dcc6ceed72452277f
Filipi Inacio Santos <filipiinacio25251120@gmail.com>
2026-08-20 11:19:15 -0300

    refactor: update name of months

--- lib/features/motorista/home/presentation/utils/semana_util.dart
+++ lib/features/motorista/home/presentation/utils/semana_util.dart
@@ -47,18 +47,18 @@ class SemanaUtil {
   }
 
   static const _meses = [
-    'janeiro',
-    'fevereiro',
-    'março',
-    'abril',
-    'maio',
-    'junho',
-    'julho',
-    'agosto',
-    'setembro',
-    'outubro',
-    'novembro',
-    'dezembro',
+    'jan',
+    'fev',
+    'mar',
+    'abr',
+    'mai',
+    'jun',
+    'jul',
+    'ago',
+    'set',
+    'out',
+    'nov',
+    'dez',
   ];
 
   static const _diasSemana = [
